9f7cc55917e25384337489e170c047ab71d85bfb
Momcilo <s315282@stud.hioa.no>
2019-10-07 04:43:59 +0200

    Adding routes, coding the clock and adjust functions

---
+++ DateTimePickerFlex.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import {StyleSheet, Button, View, Text, TouchableOpacity } from "react-native";
+import DateTimePicker from "react-native-modal-datetime-picker";
+import moment from "moment";
+import { white, bold } from "ansi-colors";
+
+export default class DateTimePickerFlex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDateTimePickerVisible: false,
+      time: moment(new Date()).format("HH:mm"),
+      format: "24H"
+    };
+  }
+
+
+  showDateTimePicker = () => {
+    this.setState({ isDateTimePickerVisible: true });
+  };
+ 
+  hideDateTimePicker = () => {
+    this.setState({ isDateTimePickerVisible: false });
+  };
+ 
+  handleDatePicked = date => {
+    this.setState({
+      isDateTimePickerVisible: false,
+      time: this.formatTime(date)
+    })
+  };
+
+  handleFormat = format => {
+    let time = this.state.time;
+
+    if(this.state.format === "24H"){
+      time = moment(time, "HH:mm")
+    }else if(this.state.format ==="12H"){
+      time = moment(time, "HH:mm a");
+    }
+    
+
+    if(format === "12H"){
+      this.setState({ format: format, time: moment(time).format("hh:mm A") })
+
+    }else if(format ==="24H"){
+      this.setState({format: format, time: moment(time).format("HH:mm")})
+
+  
+    }    
+  }
+  formatTime = timePicked => {
+    const format = this.state.format;
+    if(format === "24H"){
+      return moment(timePicked).format("HH:mm")
+    }else if(format === "12H"){
+      return moment(timePicked).format("hh:mm A")
+    }
+  }
+
+ 
+  render() {
+    return (
+      <>
+      <View style={styles.container}>
+
+      <View style={styles.container2}>
+      <Text style={styles.titleText}>{this.state.time}</Text>
+      </View>
+      <TouchableOpacity style={styles.timeButton} title="Velg tidspunkt" onPress={this.showDateTimePicker}>
+        <Text style={styles.buttonTxt}>Velg tidspunkt</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this.handleDatePicked}
+          onCancel={this.hideDateTimePicker}
+          mode="time"
+          confirmTextIOS="Bekreft"
+          cancelTextIOS="Avbryt"
+          titleIOS="Velg et klokkeslett"
+        
+        />
+
+        <View style={styles.container2}>
+        <TouchableOpacity style={styles.formatButton} title="12H" onPress={this.handleFormat.bind(this, "12H")}>
+        <Text style={styles.buttonTxt}>12H</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.formatButton} title="24H" onPress={this.handleFormat.bind(this, "24H")}>
+        <Text style={styles.buttonTxt}>24H</Text>
+        </TouchableOpacity>
+        </View>
+
+        </View>
+      </>
+    );
+  }
+}
